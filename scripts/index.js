d4afe0ce24d78540878d0fe3082dc2fc58dbf36b
matt-colen <mzcolen@gmail.com>
2024-05-02 15:12:55 -0500

    Updates fetchMovieData function

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,26 @@
-document.getElementById("search-form").addEventListener("submit", (e) => {
+document.getElementById("search-form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  const searchInput = document.getElementById("search-bar__input");
-  fetchMovieData(searchInput.value).then((data) => {
+  const searchInput = document.getElementById("search-bar__input").value;
+  try {
+    const data = await fetchMovieData(searchInput);
     console.log(data);
     // Do something with the movie data
-  });
+  } catch (error) {
+    console.error(error);
+  }
 });
 
 // Function to fetch movie data
 const fetchMovieData = async (title) => {
-  const res = await fetch(
-    `../functions/movieSearch.js?title="${encodeURIComponent(title)}`
-  );
-  const data = await res.json();
-  return data;
+  try {
+    const response = await fetch(
+      `/.netlify/functions/movieSearch?title=${encodeURIComponent(title)}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch movie data");
+    }
+    return await response.json();
+  } catch (error) {
+    throw new Error("Failed to fetch movie data");
+  }
 };
