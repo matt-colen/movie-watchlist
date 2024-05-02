7815b4a52d37818de2b7d3cc28727342efb15589
matt-colen <mzcolen@gmail.com>
2024-05-02 14:31:53 -0500

    Adds netlify serverless function structure

---
+++ functions/movieSearch.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+exports.handler = async (event, context) => {
+  try {
+    const { title } = event.queryStringParameters;
+    const OMDB_API_KEY = process.env.OMDB_API_KEY; // Access environment variable
+
+    // Make request to third-party API using axios
+    const axiosResponse = await axios.get(
+      `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}`
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(axiosResponse.data),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Internal Server Error" }),
+    };
+  }
+};
