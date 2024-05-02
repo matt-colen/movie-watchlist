4de98a9299cae44becc3050f8c60ea65dd1be677
matt-colen <mzcolen@gmail.com>
2024-05-02 17:29:33 -0500

    Updates netlify serverless function

--- functions/movieSearch.js
+++ functions/movieSearch.js
@@ -1,24 +1,24 @@
-import axios from "axios";
+const axios = require("axios");
 
-exports.handler = async (event) => {
+const handler = async (event) => {
+  const { title } = event.queryStringParameters;
+  const OMDB_API_KEY = process.env.OMDB_API_KEY; // Access environment variable
+  const url = `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}`;
+
   try {
-    const { title } = event.queryStringParameters;
-    const OMDB_API_KEY = process.env.OMDB_API_KEY; // Access environment variable
-
-    // Make request to third-party API using axios
-    const axiosResponse = await axios.get(
-      `http://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}`
-    );
+    const { data } = await axios.get(url);
 
     return {
       statusCode: 200,
-      body: JSON.stringify(axiosResponse.data),
+      body: JSON.stringify(data),
     };
-  } catch (error) {
-    console.error(error);
+  } catch (e) {
+    const { status, statusText, headers, data } = e.response;
     return {
-      statusCode: 500,
-      body: JSON.stringify({ error: "Internal Server Error" }),
+      statusCode: 200,
+      body: JSON.stringify({ status, statusText, headers, data }),
     };
   }
 };
+
+module.exports = { handler };
--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,15 @@
-document.getElementById("search-form").addEventListener("submit", async (e) => {
-  e.preventDefault();
+document.getElementById("search-form").addEventListener("submit", (e) => {
   const searchInput = document.getElementById("search-bar__input").value;
-  try {
-    const data = await fetchMovieData(searchInput);
-    console.log(data);
-    // Do something with the movie data
-  } catch (error) {
-    console.error(error);
-  }
+
+  e.preventDefault();
+  renderMovies(searchInput);
 });
 
 // Function to fetch movie data
 const fetchMovieData = async (title) => {
   try {
     const response = await fetch(
-      `/.netlify/functions/movieSearch?title=${encodeURIComponent(title)}`
+      `/.netlify/functions/movieSearch?title=${title}`
     );
     if (!response.ok) {
       throw new Error("Failed to fetch movie data");
@@ -22,5 +17,16 @@ const fetchMovieData = async (title) => {
     return await response.json();
   } catch (error) {
     throw new Error("Failed to fetch movie data");
+  }
+};
+
+// Function to render movie data
+const renderMovies = async (input) => {
+  try {
+    const data = await fetchMovieData(input);
+    console.log(data);
+    // Do something with the movie data
+  } catch (error) {
+    console.error(error);
   }
 };
