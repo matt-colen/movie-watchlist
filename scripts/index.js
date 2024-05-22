dbfb9726a1e2c2bcbb64d142e3277d4ea27ad879
matt-colen <mzcolen@gmail.com>
2024-05-21 21:34:12 -0500

    Adds movie details lookup feature
    Adds new fetch calls for the movies that come back from the user search input.

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,94 @@
+const results = document.getElementById("results");
+
 document.getElementById("search-form").addEventListener("submit", (e) => {
   const searchInput = document.getElementById("search-bar__input").value;
   e.preventDefault();
   renderMovies(searchInput);
 });
 
-// Fetch movie data
-const fetchMovieData = async (title) => {
+// Fetch movie results
+const fetchMovieResults = async (userInput) => {
   try {
-    const res = await fetch(`/.netlify/functions/movieSearch?title=${title}`);
-    return await res.json();
+    const res = await fetch(
+      `/.netlify/functions/movieSearch?title=${userInput}`
+    );
+    const data = await res.json();
+
+    if (data.Response === "True") {
+      return fetchMovieDetails(data);
+    } else {
+      results.innerHTML = `<p>No movies found</p>`;
+    }
   } catch (e) {
     console.error(e);
   }
 };
 
+// Fetch movie details for each result
+const fetchMovieDetails = async (movies) => {
+  const movieData = movies.Search.map(async (movie) => {
+    const movieId = movie.imdbID;
+    const movieDetailsUrl = `/.netlify/functions/movieDetails?id=${movieId}`;
+    const movieDetailsResponse = await fetch(movieDetailsUrl);
+    return await movieDetailsResponse.json();
+  });
+
+  return await Promise.all(movieData);
+};
+
+// Clears previous search results
+const clearPreviousSearch = () => (results.innerHTML = "");
+
 // Render movie data
 const renderMovies = async (input) => {
-  const data = await fetchMovieData(input);
-  console.log(data);
+  const movieData = await fetchMovieResults(input);
+  let html = "";
+
+  clearPreviousSearch();
+
+  movieData.forEach((movie) => {
+    html += `
+    <div class="movie grid">
+      <img
+        class="movie__poster"
+        src="${movie.Poster}"
+        alt="Move poster for ${movie.Title}"
+      />
+      <div class="movie__details">
+        <div class="movie__details--primary flex">
+          <h2 class="movie-title">${movie.Title}</h2>
+          <div class="movie-rating__container flex">
+            <img
+              class="icon--favorite"
+              src="assets/icons/star.svg"
+              alt="Star icon"
+            />
+            <p id="movie-rating" class="movie-rating">${movie.imdbRating}</p>
+          </div>
+        </div>
+        <div class="movie__details--secondary flex">
+          <p id="movie-length" class="movie-length">${movie.Runtime}</p>
+          <p id="movie-genres" class="movie-genres">
+            ${movie.Genre}
+          </p>
+          <button class="btn btn--watchlist flex">
+            <img
+              class="icon--watchlist"
+              src="assets/icons/watchlist.svg"
+              alt="Add to watchlist icon"
+            />
+            Watchlist
+          </button>
+        </div>
+        <div id="movie__details--text" class="movie__details--text">
+          <p>
+            ${movie.Plot}
+          </p>
+        </div>
+      </div>
+    </div>
+    `;
+  });
+
+  results.innerHTML = html;
 };
