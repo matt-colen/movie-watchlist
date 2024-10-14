d47308d083cd643190c6f4fde9d6b53970f51d23
matt-colen <mzcolen@gmail.com>
2024-10-14 15:15:40 -0500

    Adds spinner loading icon

--- src/components/SearchLoading.jsx
+++ src/components/SearchLoading.jsx
@@ -1,5 +1,11 @@
+import { FaSpinner } from "react-icons/fa";
 import "./SearchLoading.css";
 
 export default function SearchLoading() {
-  return <h2 className="loading-header">Loading...</h2>;
+  return (
+    <h2 className="loading-header flex">
+      <FaSpinner className="spinner"/>
+      Loading
+    </h2>
+  );
 }
